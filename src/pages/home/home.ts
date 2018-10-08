8c452ed276098c1d852a8543f879c72c7b687943
skd1993 <shobhit.kr@outlook.com>
2018-10-08 21:20:27 +0530

    added mins, marks, response array

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,21 +10,34 @@ export class HomePage {
 
   questions: number;
   options: number;
+  minutes: number;
+  positive: number;
+  negative: number;
 
   constructor(public navCtrl: NavController) {
+    this.questions = 10;
+    this.options = 4; //ABCD
+    this.minutes = 20;
+    this.positive = 1;
+    this.negative = 1; 
   }
 
   ngOnInit() {
-    // this.options = 4;
-    // this.questions = 10;
+    
   }
 
   startTest() {
     const questions = this.questions
     const options = this.options 
+    const minutes = this.minutes;
+    const positive = this.positive;
+    const negative = this.negative;
     this.navCtrl.push(TestPage, {
       questions,
-      options
+      options,
+      minutes,
+      positive,
+      negative
     });
   }
 
--- src/pages/test/test.ts
+++ src/pages/test/test.ts
@@ -8,31 +8,39 @@ import { NavController, AlertController, Navbar, NavParams } from 'ionic-angular
 export class TestPage {
 
   @ViewChild(Navbar) navBar: Navbar;
- 
+
   questions: number;
   options: number;
+  minutes: number;
+  positive: number;
+  negative: number;
+
   o = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
+  answers = []
 
   constructor(public navCtrl: NavController, public alertCtrl: AlertController, public navParams: NavParams) {
     this.questions = this.navParams.get('questions');
     this.options = this.navParams.get('options');
+    this.minutes = this.navParams.get('minutes');
+    this.positive = this.navParams.get('positive');
+    this.negative = this.navParams.get('negative');
   }
 
   ngOnInit() {
-    
   }
 
   ionViewDidLoad() {
     this.setBackButtonAction()
-}
-
-setBackButtonAction() {
-  this.navBar.backButtonClick = () => {
-    return this.showConfirm()
   }
-}
 
-showConfirm() {
+  setBackButtonAction() {
+    this.navBar.backButtonClick = () => {
+      return this.showConfirm()
+    }
+  }
+
+  showConfirm() {
     const confirm = this.alertCtrl.create({
       title: 'Exit test',
       message: 'Do you want to exit the test?',
@@ -53,6 +61,10 @@ showConfirm() {
       ]
     });
     confirm.present();
+  }
+
+  mcqAnswer(x, i) {
+    this.answers[x] = { 'res': i }
   }
 
 }
